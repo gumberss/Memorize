281d26f05f042dcff42baecc2eb784890874bfca
Gustavo Marin Suppi <gustavomarinsuppi@gmail.com>
2019-01-12 16:14:58 -0200

    feat: init creation of SingleDeckDetails

--- views/SingleDeck.js
+++ views/SingleDeck.js
@@ -1,25 +1,50 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { View, Text, StyleSheet, FlatList } from 'react-native'
+import { gray } from '../utils/colors';
 
 class SingleDeck extends Component {
+
     render() {
+
+        const { deck } = this.props
+
         return (
-            <View>
-                <Text>Single Deck</Text>
+            <View style={styles.deckContent}>
+                <Text style={styles.deckName} >{deck.name}</Text>
+                <Text style={styles.deckCardCount} >cards count</Text>
             </View>
         )
     }
 }
 
-// const mapStateToProps = state => ({});
+const mapStateToProps = ({ decks }, { navigation }) => {
 
-// const mapDispatchToProps = dispatch =>
-//   bindActionCreators(Actions, dispatch);
+    const { deckName } = navigation.state.params
+
+    return {
+        deck: decks[deckName]
+    }
+};
+
+const styles = StyleSheet.create({
+    deckContent: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    deckName: {
+        fontSize: 28
+    },
+    deckCardCount: {
+        fontSize: 14,
+        color: gray
+    }
+})
 
 SingleDeck.route = 'SingleDeck'
 
 export default connect(
-    // mapStateToProps,
+    mapStateToProps,
     // mapDispatchToProps
 )(SingleDeck);
